1a5f9795fac17a53ec7b94034d6198eb2828a66c
Gabriel Anguiano <anguianog97@gmail.com>
2021-02-16 10:39:54 -0800

    installed a library for hamburger menu nav bar structure complete in mark up

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-console.log('its working');
+const burger = document.querySelector('.hamburger');
+
+burger.addEventListener('click', (e) => {
+	e.preventDefault();
+	burger.classList.toggle('is-active');
+});
